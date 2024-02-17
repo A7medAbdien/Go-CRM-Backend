829a421806c9a186f06935d39860694a13ed2e3b
A7medAbdien <ahmed.g.abdien@gmail.com>
2024-02-17 06:43:56 +0300

    lead.go - init

---
+++ lead/lead.go
@@ -0,0 +1,19 @@
+package lead
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+type Lead struct {
+	gorm.Model
+	Name    string `json:"name"`
+	Company string `json:"company"`
+	Email   string `json:"email"`
+	Phone   int    `json:"phone"`
+}
+
+func GetLeads()   {}
+func GetLead()    {}
+func NewLead()    {}
+func DeleteLead() {}
